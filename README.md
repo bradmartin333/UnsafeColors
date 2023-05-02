27b76da87af124bde784ffc647b545bde405c152
Brad Martin <BMartin@parata.com>
2023-05-02 12:00:16 -0400

    working window

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # UnsafeColors
 Updating an image with a Color array pointer using Raylib
+
+## Notes
+- Make sure unsafe code is turned on in the project settings
--- UnsafeColors/UnsafeColors/Program.cs
+++ UnsafeColors/UnsafeColors/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_CsLo;
+using static Raylib_CsLo.Raylib;
+
+internal class Program
+{
+    private const int WID = 500;
+    private const int HGT = 500;
+
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        Color[] colors = new Color[WID * HGT];
+
+        new Thread(() =>
+        {
+            unsafe
+            {
+                fixed (Color* colorPtr = &colors[0])
+                {
+                    RayLoop(colorPtr);
+                }
+            }
+        }).Start();
+
+
+    }
+
+    unsafe private static void RayLoop(Color* ptr)
+    {
+        SetTraceLogLevel((int)TraceLogLevel.LOG_WARNING);
+        InitWindow(WID, HGT, "Unsafe Colors Demo");
+        Image image = GenImageColor(WID, HGT, MAROON);
+        Texture texPattern = LoadTextureFromImage(image);
+        SetTextureFilter(texPattern, TextureFilter.TEXTURE_FILTER_BILINEAR);
+        while (!WindowShouldClose())
+        {
+            BeginDrawing();
+            ClearBackground(BLACK);
+            UpdateTexture(texPattern, ptr);
+            DrawTexture(texPattern, 0, 0, WHITE);
+            EndDrawing();
+        }
+        CloseWindow();
+    }
+}
